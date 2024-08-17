3b878c9cf8cd87064969bb23955c69fc621934cc
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-08-17 06:59:09 +0530

    listner added

---
+++ src/java/model/Listner_1.java
@@ -0,0 +1,18 @@
+package model;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class Listner_1 implements ServletContextListener{
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        
+    }
+    
+}
