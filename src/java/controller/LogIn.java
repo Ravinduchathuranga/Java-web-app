276926d21398e752640258a4cbcb5edb25691df2
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-08-17 14:11:16 +0530

    sign in backend updated

--- src/java/controller/LogIn.java
+++ src/java/controller/LogIn.java
@@ -2,17 +2,33 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.User;
 
 public class LogIn extends HttpServlet {
-    
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String mobile = req.getParameter("mobile");
-        resp.getWriter().write(mobile);
+
+        ServletContext scc = req.getServletContext();
+        ArrayList<User> userList = (ArrayList<User>) scc.getAttribute("userList");
+        boolean userFound = false;
+        for (User user : userList) {
+            if (user.getMobile().equals(mobile)) {
+                userFound = true;
+                resp.sendRedirect("home.jsp");
+                break;
+            }
+        }
+        if(!userFound){
+        resp.sendRedirect("signUp.jsp");
+        }
     }
-    
+
 }
